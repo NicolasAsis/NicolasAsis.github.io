63636a0f5d11a0b0f074cd95c266657766c19ed9
NicolasAsis <nico.asis15@gmail.com>
2019-05-14 22:35:27 -0700

    Added Portfolio

---
+++ Portolio/js/portfolio.js
@@ -0,0 +1,21 @@
+function GoHome(){
+    location.href = "../index.html";
+}
+
+function GocmPage(){
+    location.href = "../CMpage.html"
+}
+
+//function ToolExpand(){
+//    var tools = document.querySelectorAll(".tools");
+//    
+//    tools[].style.transform = "scale(1.1)";
+//    tools[].style.boxShadow = "0px 8px 6px -6px black";
+//}
+//
+//function ToolNormal(){
+//    var tools = document.querySelectorAll(".tools");
+//    
+//    tools[].style.transform = "scale(1)";
+//    tools[].style.boxShadow = "none";
+//}
